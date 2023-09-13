f8ab3ccdddfd4738572b19d8428562564a164f06
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-09-13 10:36:22 +0000

    n

--- .devcontainer/basics/variables.js
+++ .devcontainer/basics/variables.js
@@ -6,8 +6,10 @@ var vs const
 The difference between let and var is in the scope of the variables they create: Variables declared by let are only available inside the block where they're defined. Variables declared by var are available throughout the function in which they're declared.
 */
 let name = "vinayak";
+/// when u pass without assigning any value it show un defined
+let billo
 
 const relation = " friends"// we cant change const value 
 var id = "25678"
 //// prit all these using
-console.table([name,relation,id]);
+console.table([name,relation,id,billo]);
