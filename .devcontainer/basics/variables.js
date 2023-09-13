58c3a8514dcd151293352cfe02aaa8298277d634
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-09-13 10:34:06 +0000

    new

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,13 @@
+/*
+there are thre types of variables in js let const var.....
+const cant be changed 
+var is the older 
+var vs const 
+The difference between let and var is in the scope of the variables they create: Variables declared by let are only available inside the block where they're defined. Variables declared by var are available throughout the function in which they're declared.
+*/
+let name = "vinayak";
+
+const relation = " friends"// we cant change const value 
+var id = "25678"
+//// prit all these using
+console.table([name,relation,id]);
