58858166e562b1aa967a446617f49e822758d57d
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-09-16 13:13:30 +0000

    h

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,9 @@
+"use-strict" /// treat all js  code as newer version 
+// data types
+let age = 18
+let name = " vinayak"
+let isadmin = true
+/// i decalared a variable but didnt assign it a value so its is undefined 
+let state;
+
+
