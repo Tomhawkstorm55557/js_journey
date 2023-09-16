ca552a74fc3a77764e539f63d6a6c8b1428f8751
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-09-16 13:30:23 +0000

    h

---
+++ .devcontainer/basics/dataconversion.js
@@ -0,0 +1,10 @@
+//let score = "vinayak";
+let score = 45;
+/// for ex u want an score now score must be in int 
+let vaue = Number(score);
+console.log(vaue);
+//let isval = 1;
+//let isval = 76;
+let isval =0;
+let tr = Boolean(isval);
+console.log(tr);
