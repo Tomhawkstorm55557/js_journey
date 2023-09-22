a4c2aa84e5a05f97bba86423b5dbb22caab3e652
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-09-22 13:35:33 +0000

    j

---
+++ .devcontainer/basics/string.js
@@ -0,0 +1,26 @@
+name = " vinayak"
+score = 590
+console.log('his name is ${name}');
+console.log(`his name is ${name}`);
+const game = new String('gta')
+console.log(game[0]);
+console.log(game.length);
+console.log(game.toLowerCase());
+console.log(game.toUpperCase());
+//console.log(game.charAt(A));
+console.log(game.charAt(2));
+console.log(game.indexOf('t'));
+const latest = new String('grand theft auto');
+//const subs = latest.substring(0,6);
+//console.log(subs);
+/// slice dont work where there is space in string
+const est = "your_string_here"; // Replace with your actual string
+const older = est.slice(-8, -6);
+console.log(older);
+const haat = "    vinayak superhero ";
+console.log(haat.trim());
+const url = "https.stupidvinayak.com";
+console.log(url.replace('stupid','superhero'));
+console.log(url.includes('vinayak'));
+
+
