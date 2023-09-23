4ae5e2c7ebe3f8a1cac31254e0a6c92063bba5b9
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-09-23 18:12:24 +0000

    hh

---
+++ .devcontainer/basics/number_math.js
@@ -0,0 +1,20 @@
+ const  score = 400;
+ console.log(score);
+ const balance = new Number(100)
+ console.log(balance);
+ console.log(toString(balance).length);
+ console.log(balance.toFixed(3));
+ const n1 = 12234.64748382
+ console.log(n1.toPrecision(5));
+ const lakh = 100000
+ console.log(lakh.toLocaleString());
+console.log(Math.max(1,45,43,6));
+console.log(Math.random());
+const min = 10;
+const max = 20;
+/// floor-->> round off
+console.log(Math.floor(Math.random()*(max-min))+min);
+
+
+
+
---
+++ .devcontainer/interview/question2.js
@@ -0,0 +1,6 @@
+let c = [];
+let d =c;
+console.log(d==c);
+console.log(d===c);
+
+/// kyuki memory address same hai 
